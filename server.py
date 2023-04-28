b327b882b582945f16364bc4a94abcb2f650e36e
Sodev34 <111426977+Sodev34@users.noreply.github.com>
2023-04-28 10:45:30 +0200

    PEP 8

--- server.py
+++ server.py
@@ -30,23 +30,23 @@ def clubs_table():
 
 
 @app.route("/showSummary", methods=["POST"])
-def showSummary():
+def show_summary():
     try:
         club = [club for club in clubs if club["email"] == request.form["email"]][0]
         d_competitions = competition_date(competitions)
         return render_template("welcome.html", club=club, competitions=d_competitions)
     except IndexError:
-        flash("Unknown Email")    
-        return render_template('index.html'), 403
+        flash("Unknown Email")
+        return render_template("index.html"), 403
 
 
 @app.route("/book/<competition>/<club>")
 def book(competition, club):
-    foundClub = [c for c in clubs if c["name"] == club][0]
-    foundCompetition = [c for c in competitions if c["name"] == competition][0]
-    if foundClub and foundCompetition:
+    found_club = [c for c in clubs if c["name"] == club][0]
+    found_competition = [c for c in competitions if c["name"] == competition][0]
+    if found_club and found_competition:
         return render_template(
-            "booking.html", club=foundClub, competition=foundCompetition
+            "booking.html", club=found_club, competition=found_competition
         )
     else:
         flash("Something went wrong-please try again")
@@ -54,33 +54,37 @@ def book(competition, club):
 
 
 @app.route("/purchasePlaces", methods=["POST"])
-def purchasePlaces():
+def purchase_places():
     competition = [c for c in competitions if c["name"] == request.form["competition"]][
         0
     ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
-    placesRequired = int(request.form["places"])
-    if placesRequired < 0 or placesRequired > int(club["points"]):
+    places_required = int(request.form["places"])
+    if places_required < 0 or places_required > int(club["points"]):
         flash("enter a valid number")
         return redirect(
             url_for("book", club=club["name"], competition=competition["name"])
         )
-    elif placesRequired > 12:
+    elif places_required > 12:
         flash("You can reserve a maximum of 12 places in a competition.")
         return render_template("booking.html", club=club, competition=competition)
-    elif int(competition["numberOfPlaces"]) < placesRequired:
+    elif int(competition["numberOfPlaces"]) < places_required:
         flash("There are not enough places available for this competition.")
         return render_template("booking.html", club=club, competition=competition)
     else:
         try:
             update_comp_reserved_places(
-                competition, club, placesRequired, places_reserved
+                competition, club, places_required, places_reserved
             )
             competition["numberOfPlaces"] = (
-                int(competition["numberOfPlaces"]) - placesRequired
+                int(competition["numberOfPlaces"]) - places_required
             )
-            club["points"] = int(club["points"]) - placesRequired
-            flash("Great-booking complete! You have reserved {} seats.".format(placesRequired))
+            club["points"] = int(club["points"]) - places_required
+            flash(
+                "Great-booking complete! You have reserved {} seats.".format(
+                    places_required
+                )
+            )
             return render_template("welcome.html", club=club, competitions=competitions)
 
         except ValueError:
